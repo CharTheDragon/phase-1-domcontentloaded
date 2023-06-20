9c28edd07b8ec9c9e59838199a6982dadd2d70ca
Nathaniel Swan <cygnet1.999@gmail.com>
2023-06-19 19:20:19 -0500

    changed text id to contain this is really cool!

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const words = document.querySelector('#text');
+  words.textContent = "This is really cool!"
+  console.log(words);
+});
